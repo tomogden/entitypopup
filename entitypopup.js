854bf754e00daa51a45f52df10fdc6cc628c1187
tomogden <drupal@theogdengroup.net>
2012-07-17 10:52:45 -0400

    This is the first working version of this module from January 27, 2012. It utilizes Chaos Tools for some functions, rather than trying to do it from scratch, as in a previous non-functining version.

--- entitypopup.js
+++ entitypopup.js
@@ -1,69 +1,20 @@
 /**
- * function addEvent
- * Also works on browsers that don't support addEventListener (IE) 
- */
-var addEvent = (function () {
-  if (document.addEventListener) {
-    return function (el, type, fn) {
-      if (el && el.nodeName || el === window) {
-        el.addEventListener(type, fn, false);
-      } else if (el && el.length) {
-        for (var i = 0; i < el.length; i++) {
-          addEvent(el[i], type, fn);
-        }
-      }
-    };
-  } else {
-    return function (el, type, fn) {
-      if (el && el.nodeName || el === window) {
-        el.attachEvent('on' + type, function () { return fn.call(el, window.event); });
-      } else if (el && el.length) {
-        for (var i = 0; i < el.length; i++) {
-          addEvent(el[i], type, fn);
-        }
-      }
-    };
-  }
-})();
-
- 
- /**
- * Set Popup
- * Binds popup to the 'click' event to each respective element found with the 'entitypopup' class.
+ * Set modal dialog element.
  */ 
-  (function ($) {
-    Drupal.behaviors.entitypopup_popup = {
-      attach: function (context, settings) {
-        /* Setup Popup Elements **************************************/
-          var links = document.querySelectorAll(".entitypopup"), containerElement = null; // querySelectorAll is IE8+ compatible
+  Drupal.theme.prototype.entitypopup = function () {
+    var html = ''
+    html += '  <div id="ctools-modal">'
+    html += '    <div class="ctools-modal-content">' // panels-modal-content
+    html += '      <div class="modal-header">';
+    html += '        <a class="close" href="#">';
+    html +=            Drupal.CTools.Modal.currentSettings.closeText + Drupal.CTools.Modal.currentSettings.closeImage;
+    html += '        </a>';
+    html += '        <span id="modal-title" class="modal-title"> </span>';
+    html += '      </div>';
+    html += '      <div id="modal-content" class="modal-content">';
+    html += '      </div>';
+    html += '    </div>';
+    html += '  </div>';
 
-          for (var i = 0; i < links.length; i++) {
-            containerElement = links[i];
-            var view_mode = 'teaser';
-            var classArray = containerElement.className.split(' ');
-            for (var j = 0; j < classArray.length; j++) {
-              testArray = classArray[j].split('-');
-              if (testArray[0] == 'entitypopup' && testArray.length > 1) {
-                view_mode = testArray[1];
-              }
-            }
-            
-            addEvent(containerElement, 'click', function () {
-              var url = '/entitypopup/'+this.id.replace('-','/')+'/'+view_mode;
-              $.get(url, function(response) {
-                response = $.parseJSON(response);
-          // Insert new element for modal popup.
-                if(response){
-                  var modalElement = $(response);
-                  modalElement.insertAfter(document.querySelector('#skip-link'));
-                }
-                
-                $('#container-count-'+response['nid']).replaceWith(response['element']);
-              });
-            });
-          }
-
-      }
-    };
-  }(jQuery));
-  
+    return html;
+  }
